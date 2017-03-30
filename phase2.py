421257844bc1a6ca18014c8f83d67b1270f3463c
Your Name <you@example.com>
2017-03-30 13:15:12 -0600

    added ./break commands that probably work

--- phase2.py
+++ phase2.py
@@ -6,4 +6,7 @@ os.system("sort -u -o dates.txt dates.txt")
 os.system("perl break.pl < tweets.txt > tweetsOP.txt")
 os.system("perl break.pl < terms.txt > termsOP.txt")
 os.system("perl break.pl < dates.txt > dateOP.txt")
+os.system("db_load -T -t hash -f tweetsOP.txt tweets.db")
+os.system("db_load -T -t btree -f termsOP.txt terms.db")
+os.system("db_load -T -t btree -f dateOP.txt dates.db")
 
