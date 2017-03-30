2711036183e5bbe4435e714dd420bee58f00df7a
Your Name <you@example.com>
2017-03-30 13:36:15 -0600

    relabelled the output files to the expected tw/te/da.idx

--- phase2.py
+++ phase2.py
@@ -6,14 +6,14 @@ os.system("sort -u -o dates.txt dates.txt")
 os.system("perl break.pl < tweets.txt > tweetsOP.txt")
 os.system("perl break.pl < terms.txt > termsOP.txt")
 os.system("perl break.pl < dates.txt > dateOP.txt")
-os.system("db_load -T -t hash -f tweetsOP.txt tweets.db")
-os.system("db_load -T -t btree -f termsOP.txt terms.db")
-os.system("db_load -T -t btree -f dateOP.txt dates.db")
+os.system("db_load -T -t hash -f tweetsOP.txt tw.idx")
+os.system("db_load -T -t btree -f termsOP.txt te.idx")
+os.system("db_load -T -t btree -f dateOP.txt da.idx")
 
 #######################
 # extra db_dump for verification
 
-os.system("db_dump -k -d a -f tweetdecomp.txt tweets.db")
-os.system("db_dump -k -d a -f termdecomp.txt terms.db")
-os.system("db_dump -k -d a -f datedecomp.txt dates.db")
+os.system("db_dump -k -d a -f tweetdecomp.txt tw.idx")
+os.system("db_dump -k -d a -f termdecomp.txt te.idx")
+os.system("db_dump -k -d a -f datedecomp.txt da.idx")
 
