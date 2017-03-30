b335cb32cadcdcb8cf95c3a2508e0c6edef1168d
Taylor Bos <bos@cs.ualberta.ca>
2017-03-30 14:52:06 -0600

    exact date works

--- p3dates.py
+++ p3dates.py
@@ -1,34 +1,33 @@
 from bsddb3 import db
-#def openDateDB():
-database = db.DB()
-database.open("dates.db")
-cur = database.cursor()
 
-#def closeDateDB():
- #   cur.close()
-  #  database.close()
+def openDateDB():
+    database = db.DB()
+    database.open("da.idx")
+    cur = database.cursor()
+    return database, cur
+
+def closeDateDB(database, cur):
+    cur.close()
+    database.close()
 
 #date is a string
 #returns a list of ids as byte literals
 def exactDate(date):
-    #openDateDB()
+    database, cur = openDateDB()
     ids = []
     iter = cur.first()
     while iter:
         idate = iter[0]
-        print(idate.decode("utf-8"))
+        #print(idate.decode("utf-8"))
         if idate.decode("utf-8") == date:
             ids.append(iter[1])
         iter = cur.next()
-    #closeDateBD()
+    closeDateDB(database, cur)
     return ids
 
 ids = exactDate("2012/03/11")
 print(len(ids))
 for id in ids:
     print(id.decode("utf-8"))
-
-cur.close()
-database.close()
     
             
