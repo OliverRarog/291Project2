9db2aa2c6d988ba64513ff9febe7a1156064eb49
OliverRarog <oliverrarog@gmail.com>
2017-04-02 20:52:44 -0600

    just need wildcards functions/testing

--- phase3.py
+++ phase3.py
@@ -1,40 +1,40 @@
 import xml.etree.ElementTree as ET
 import re, sys, p3terms, p3dates
-#from bsddb3 import db
+from bsddb3 import db
 
 #id as a byte literal
 #finds the data for the ids given
 #prints the data
 #probably needs to be debugged
-# def results(bid):
-#     database = db.DB()
-#     database.open("tw.idx")
-#     cur = database.cursor()
-#     iter = cur.first()
-#     while iter:
-#         iid = iter[0]
-#         if iid == bid:
-#             root = ET.fromstring(iter[1].decode("utf-8"))
-#             for child in root.iter():
-#                 if(child.text != None):
-#                     if(child.tag == 'id'):
-#                         print("Id: " + child.text)
-#                     if(child.tag == 'created_at'):
-#                         print("Created at: " + child.text)
-#                     if(child.tag == 'text'):
-#                         print("Text: " + child.text)
-#                     if(child.tag == 'name'):
-#                         print("Name: " + child.text)
-#                     if(child.tag == 'location'):
-#                         print("Location: " + child.text)
-#                     if(child.tag == 'description'):
-#                         print("Description: " + child.text)
-#                     if(child.tag == 'url'):
-#                         print("URL: " + child.text)
-#             print("")
-#         iter = cur.next()
-#     cur.close()
-#     database.close()
+def results(bid):
+    database = db.DB()
+    database.open("tw.idx")
+    cur = database.cursor()
+    iter = cur.first()
+    while iter:
+        iid = iter[0]
+        if iid == bid:
+            root = ET.fromstring(iter[1].decode("utf-8"))
+            for child in root.iter():
+                if(child.text != None):
+                    if(child.tag == 'id'):
+                        print("Id: " + child.text)
+                    if(child.tag == 'created_at'):
+                        print("Created at: " + child.text)
+                    if(child.tag == 'text'):
+                        print("Text: " + child.text)
+                    if(child.tag == 'name'):
+                        print("Name: " + child.text)
+                    if(child.tag == 'location'):
+                        print("Location: " + child.text)
+                    if(child.tag == 'description'):
+                        print("Description: " + child.text)
+                    if(child.tag == 'url'):
+                        print("URL: " + child.text)
+            print("")
+        iter = cur.next()
+    cur.close()
+    database.close()
 
 # checks if query is YYYY/MM/DD
 def validateDateFormat(date):
@@ -45,6 +45,15 @@ def validateDateFormat(date):
     except ValueError:
         pass
     return False
+
+def intersectResults(idArray):
+    if(len(idArray )< 1):
+        return []
+    intersected = idArray[0]
+    for array in idArray:
+        intersected = list(set(array).intersection(intersected))
+    return intersected
+
 
 def interface():
     while(1):
@@ -59,40 +68,50 @@ def interface():
             if(query.startswith("text:")):
                 query = query[5:]
                 if(query[-1] == '%'):
-                    print("wildcard text")
+                    if(not query[:-1].isalnum):
+                        print('Query text must only contain alphanumeric characters! Rejecting query text:%s' % query)
+                        continue
+
                 elif(not query.isalnum()):
-                    print('Reject query text:%s as it contains non alphanumeric characters!' % query)
+                    print('Rejecting query text:%s as it contains non alphanumeric characters!' % query)
                     continue
                 # do text matching
-                idArrays[i] = returnText(query)
+                else:
+                    idArrays[i] = p3terms.returnText(query)
                 
             
             elif (query.startswith("name:")):
                 query = query[5:]
                 if(query[-1] == '%'):
-                    print("wildcard name")
+                    if(not query[:-1].isalnum):
+                        print('Query text must only contain alphanumeric characters! Rejecting query name:%s' % query)
+                        continue
+
                 elif(not query.isalnum()):
-                    print('Reject query name:%s as it contains non alphanumeric characters!' % query)
+                    print('Rejecting query name:%s as it contains non alphanumeric characters!' % query)
                     continue
                 # do name matching
-                idArray[i] = returnName(query)
+                else:
+                    idArray[i] = p3terms.returnName(query)
                 
-            
             elif (query.startswith("location:")):
                 query = query[9:]
                 if(query[-1] == '%'):
-                    print("wildcard location")
+                    if(not query[:-1].isalnum):
+                        print('Query text must only contain alphanumeric characters! Rejecting query location:%s' % query)
+                        continue
+
                 elif(not query.isalnum()):
-                    print('Reject query location:%s as it contains non alphanumeric characters!' % query)
+                    print('Rejecting query location:%s as it contains non alphanumeric characters!' % query)
                     continue
                 # do location matching
-                idArray[i] = returnLocation(query)
+                else:
+                    idArray[i] = p3terms.returnLocation(query)
                 
-            
             elif (query.startswith("date:")):
                 query = query[5:]
                 if(not validateDateFormat(query)):
-                    print('Reject query date:%s! All dates must be in the form YYYY/MM/DD' % query)
+                    print('Rejecting query date:%s! All dates must be in the form YYYY/MM/DD' % query)
                     continue
                 # do exacte date matching
                 idArrays[i] = p3dates.exacteDate(query)
@@ -100,33 +119,37 @@ def interface():
             elif (query.startswith("date<")):
                 query = query[5:]
                 if(not validateDateFormat(query)):
-                    print('Reject query date<%s! All dates must be in the form YYYY/MM/DD' % query)
+                    print('Rejecting query date<%s! All dates must be in the form YYYY/MM/DD' % query)
                     continue
                 # do < date matching
                 idArrays[i] = p3dates.lessThanDate
                 
-            
             elif (query.startswith("date>")):
                 query = query[5:]
                 if(not validateDateFormat(query)):
-                    print('Reject query date>%s! All dates must be in the form YYYY/MM/DD' % query)
+                    print('Rejecting query date>%s! All dates must be in the form YYYY/MM/DD' % query)
                     continue
                 # do > date matching
                 idArrays[i] = p3dates.greaterThanDate
 
-                
-            
             else:
                 if(query[-1] == '%'):
                     if(not query[:-1].isalnum):
                         print('Non-prefixed queries must only contain alphanumeric characters! Rejecting query %s' % query)
                         continue
                     # do alphanum query matching with wildcard
-                    pass
+                    idArrays[i] = p3terms.returnWildcard(query[:-1])
                 elif(not query.isalnum()):
                     print('Non-prefixed queries must only contain alphanumeric characters! Rejecting query %s' % query)
                     continue
                 # do non-wildcard alphanum matching
+                else:
+                    idArrays[i] = p3terms.returnAny(query)
+            i += 1
+
+        result = intersectResults(idArrays)
+        for rs in result:
+            result(rs)
 
 
 
